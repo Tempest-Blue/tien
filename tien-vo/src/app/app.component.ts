01722b7b6e3a0fa22d4baddeae25534cb6b3ad04
Tien Vo <Tempest-Blue@users.noreply.github.com>
2021-09-07 02:58:21 -0700

    chore: updated hero banner, working on experience section

--- tien-vo/src/app/app.component.ts
+++ tien-vo/src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tien-vo';
+
+  
+  ngOnInit() {
+  }
+
+  get_years_of_experience() {
+    const current_year = new Date().getFullYear();
+    return (current_year - 2016);
+  }
+
+  open_page(website: string ) {
+    const website_mapping: {[key: string]: any} = {
+      guaranteed_rate: 'https://www.rate.com/',
+      loanDepot: 'https://www.loandepot.com/',
+      wolin_design_group: 'https://wdgcorp.com/',
+    };
+    console.log(website_mapping[website]);
+    window.open(website_mapping[website]);
+  }
 }
